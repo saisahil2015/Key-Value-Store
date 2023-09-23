13250b7af452bce4ac1fbd222f7a8c23c3fc1578
Jingxian <jingxianchai01@gmail.com>
2023-09-23 12:11:36 -0500

    added logging

--- server.py
+++ server.py
@@ -7,18 +7,18 @@ from models import db, Key_value
 app = Flask(__name__)
 
 # sqllite 
-# sqllite_uri = 'sqlite:///' + os.path.abspath(os.path.curdir) + '/key_values.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = sqllite_uri
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+sqllite_uri = 'sqlite:///' + os.path.abspath(os.path.curdir) + '/key_values.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = sqllite_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db.init_app(app)
+db.init_app(app)
 
-# # load existing db otherwise create new db
-# with app.app_context():
-#     try:
-#         Key_value.query.all()
-#     except:
-#         db.create_all()
+# load existing db otherwise create new db
+with app.app_context():
+    try:
+        Key_value.query.all()
+    except:
+        db.create_all()
 
 # logging
 logging.basicConfig(
@@ -38,19 +38,22 @@ def get_value():
     
     # find a given key in database
     if matched_key_value:
-        return {
-                'status': 'success',
-                'value': matched_key_value.value,
-                'message': 'successfully retrieved'
-                }
+        message = f'The key [{given_key}] is found'
+        app.logger.info(message)
+        return jsonify({
+            'status': 'success',
+            'value': matched_key_value.value,
+            'message': message
+        }), 200
 
     # not found
     else:
-        return {
-                'status': 'fail',
-                'message': 'the key is not found'
-                }
-
+        message = f'The key [{given_key}] is not found'
+        app.logger.warning(message)
+        return jsonify({
+            'status': 'fail',
+            'message': message
+        }), 404
 
 @app.route('/store', methods=['PUT'])
 def put_key():
@@ -61,10 +64,12 @@ def put_key():
 
     # if a given key already exists in database
     if row:
-        return {
-                'status': 'fail',
-                'message': 'the key already exists'
-                }
+        message = f'The key [{new_key}] already exists'
+        app.logger.warning(message)
+        return jsonify({
+            'status': 'fail',
+            'message': message
+        }), 404
 
     # a given key can be stored
     else:
@@ -72,10 +77,12 @@ def put_key():
         db.session.add(new_pair)
         db.session.commit()
 
-        return {
-                'status': 'success',
-                'message': 'value is stored with a given key'
-                }
+        message = f'The key [{new_key}] with the value [{new_value}] is stored'
+        app.logger.info(message)
+        return jsonify({
+            'status': 'success',
+            'message': message
+        }), 201
 
 @app.route('/remove', methods=['DELETE'])
 def remove_key():
@@ -86,25 +93,21 @@ def remove_key():
     if row:
         db.session.delete(row)
         db.session.commit()
-        
-        return {
-                'status': 'success',
-                'message': 'remove a key and value'
-                }
 
+        message = f'The key [{given_key}] is removed'
+        app.logger.info(message)
+        return jsonify({
+            'status': 'success',
+            'message': message
+        }), 200
+    
     else:
-        return {
-                'status': 'fail',
-                'message': 'the key is not found'
-                }
-
-@app.route("/get/<key>")
-def getValue(key):
-    if key in record:
-        app.logger.info(f"Found value: {record[key]}; For key: {key}")
-        return jsonify({"value": record[key]})
-    app.logger.warning(f"No value found for key {key}")
-    return jsonify({"error": f"No value found for key {key}"}), 404
+        message = f'The key [{given_key}] is not found'
+        app.logger.warning(message)
+        return jsonify({
+            'status': 'fail',
+            'message': message
+        }), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
