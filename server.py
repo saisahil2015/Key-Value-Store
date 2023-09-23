a71ea2f687d91a5ce0a886a06e57cca2fb812d40
Sahil Raina <saisahil2015@gmail.com>
2023-09-23 02:45:53 -0500

    Implemented the Logging

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.DEBUG,
+    format="%(asctime)s  : %(message)s",
+)
 
 
 record = {"Sahil": "Raina"}
@@ -9,7 +16,9 @@ record = {"Sahil": "Raina"}
 @app.route("/get/<key>")
 def getValue(key):
     if key in record:
+        app.logger.info(f"Found value: {record[key]}; For key: {key}")
         return jsonify({"value": record[key]})
+    app.logger.warning(f"No value found for key {key}")
     return jsonify({"error": f"No value found for key {key}"}), 404
 
 
