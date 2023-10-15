e48f10e86acbc47b4615f0f9ff03fbcd1d002096
Jingxian <jingxianchai01@gmail.com>
2023-10-15 15:20:47 -0500

    added r^2 score

--- models/svr.py
+++ models/svr.py
@@ -1,6 +1,6 @@
 # support vector regression
 
-from sklearn.svm import SVR
+from sklearn.svm import LinearSVR
 from sklearn.model_selection import train_test_split
 
 from utils import read_csv, validate
@@ -12,20 +12,25 @@ N_LABELS = 7
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # svr
+from sklearn.svm import LinearSVR
 svrs = []
 for i in range(N_LABELS):
-    svr = SVR(kernel='linear', C=1.0, epsilon=0.1)
+    svr = LinearSVR(C=1.0, epsilon=0.001)
     svr.fit(X_train, y_train[:, i])
     svrs.append(svr)
 
 # validation
 overall_rmse = 0
 overall_mae = 0
+overall_r2 = 0
 for i, svr in enumerate(svrs):
-    y_pred, rmse, mae = validate(svr, X_test, y_test[:, i], f'SVR {["max_memory_used", "cpu_total_tottime", "cpu_total_cumtime", "cpu_percall_tottime", "cpu_percall_cumtime", "throughput", "latency"][i]}')
+    y_pred, rmse, mae, r2 = validate(svr, X_test, y_test[:, i], f'SVR {["max_memory_used", "cpu_total_tottime", "cpu_total_cumtime", "cpu_percall_tottime", "cpu_percall_cumtime", "throughput", "latency"][i]}')
     overall_rmse += rmse
     overall_mae += mae
+    overall_r2 += r2
 
-# average RMSE and MAE
+# average RMSE, MAE, R^2
 print(f'Average RMSE: {overall_rmse / N_LABELS:.8f}')
 print(f'Average MAE: {overall_mae / N_LABELS:.8f}')
+print(f'Average R^2: {overall_r2 / N_LABELS:.8f}')
+
--- models/utils.py
+++ models/utils.py
@@ -25,4 +25,13 @@ def validate(model, X_test, y_test, model_name):
     mae = mean_absolute_error(y_test, y_pred)
     print(f"MAE: {mae:.8f}")
 
-    return y_pred, rmse, mae
+    # R^2
+    r2 = model.score(X_test, y_test)
+    print(f"R^2: {r2:.8f}")
+
+    from sklearn.metrics import mean_squared_error, mean_absolute_error
+    rmse = mean_squared_error(y_test, y_pred, squared=False) # RMSE
+    mae = mean_absolute_error(y_test, y_pred)   # MAE
+    r2 = model.score(X_test, y_test)    # R^2
+
+    return y_pred, rmse, mae, r2
