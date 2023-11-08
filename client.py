47bd4fbb592cacb47ef102978cc2bbb5619e9a91
Sahil Raina <saisahil2015@gmail.com>
2023-11-08 17:04:47 -0600

    Testing with updated client code where performance measured for each request and requests increased incrementally

--- client.py
+++ client.py
@@ -1,71 +1,62 @@
 import requests
 import time
-import threading
 import json
 import argparse
 import docker
 from consistent_hashing import HashRing
 
-# HOST, PORT = "127.0.0.1", 8000
-# BASE_URL = f"http://{HOST}:{PORT}"
 BASE_URL = "http://localhost:80"
-NUM_REQUESTS = 1000  # Number of requests to send per client
-NUM_CLIENTS = 10  # Number of concurrent clients
 
-throughputs = []
-latencies = []
 
 image_name = "docker-kv-store"
 
-# container_names = ["container1"]
-container_names = ["container1", "container2"]
+container_names = ["container1"]
+# container_names = ["container1", "container2"]
 # container_names = ["container1", "container2", "container3"]
 
 ring = HashRing(nodes=container_names)
 
 
-def client_thread(client_id):
-    # Start time
+throughputs = []
+latencies = []
+n_requests = []
+
+
+def measure(n):
     start_time = time.time()
+    for i in range(n):
+        key = f"key-{i}"
+        value = f"value-{i}"
 
-    for i in range(NUM_REQUESTS):
-        key = f"key-{client_id}-{i}"
-        value = f"value-{client_id}-{i}"
+    container_address = None
 
-        container_address = None
+    container_name = ring.get_node(key)
 
-        container_name = ring.get_node(key)
+    if container_name == "container1":
+        container_address = "http://localhost:8070"
+    elif container_name == "container2":
+        container_address = "http://localhost:8080"
+    elif container_name == "container3":
+        container_address = "http://localhost:8090"
 
-        if container_name == "container1":
-            container_address = "http://localhost:8070"
-        elif container_name == "container2":
-            container_address = "http://localhost:8080"
-        elif container_name == "container3":
-            container_address = "http://localhost:8090"
-
-        # PUT request
-        requests.put(
-            f"{container_address}/store", params={"key": key}, data={"value": value}
-        )
-
-        # GET request
-        requests.get(f"{container_address}/retrieve", params={"key": key})
-
-        # DEL request
-        requests.delete(f"{container_address}/remove", params={"key": key})
-
-    # End time
-    end_time = time.time()
-
-    elapsed_time = end_time - start_time
-    throughput = NUM_REQUESTS * 3 / elapsed_time  # Multiply by 3 for PUT, GET, DEL
-    throughputs.append(throughput)
-    latency = elapsed_time / (NUM_REQUESTS * 3)  # Average latency per request
-    latencies.append(latency)
-
-    print(
-        f"Client-{client_id} | Throughput: {throughput:.2f} req/s | Average Latency: {latency:.6f} seconds"
+    # PUT request
+    requests.put(
+        f"{container_address}/store", params={"key": key}, data={"value": value}
     )
+
+    # GET request
+    requests.get(f"{container_address}/retrieve", params={"key": key})
+
+    # DEL request
+    requests.delete(f"{container_address}/remove", params={"key": key})
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    latency = elapsed_time / (n * 3)
+    throughput = (n * 3) / elapsed_time
+
+    return latency, throughput
 
 
 if __name__ == "__main__":
@@ -79,22 +70,24 @@ if __name__ == "__main__":
 
     print(f"Testing {title}.")
 
-    clients = []
-    for i in range(NUM_CLIENTS):
-        t = threading.Thread(target=client_thread, args=(i,))
-        clients.append(t)
-        t.start()
+    for i in range(1, 301):
+        latency, throughput = measure(i)
+        latencies.append(latency)
+        throughputs.append(throughput)
+        n_requests.append(i)
 
-    for t in clients:
-        t.join()
+        print(
+            f"Number of requests: {i} | Throughput: {throughput:.2f} req/s | Average Latency: {latency:.6f} seconds"
+        )
 
     # write throughput and latency into a json file
-    json_data = {"throughput": throughputs, "latency": latencies}
+    json_data = {
+        "throughput": throughputs,
+        "latency": latencies,
+        "n_requests": n_requests,
+    }
     with open("data/" + filename, "w") as f:
         print("Writing data to", filename)
         json.dump(json_data, f)
-
-    print("Overall Throughput: ", sum(throughputs))
-    print("Overall Latency: ", sum(latencies) / NUM_CLIENTS)
 
     print("Testing completed.")
