164b7e7e4d452e1c89a3a56eadd5ba425f4a0234
Sahil Raina <saisahil2015@gmail.com>
2023-11-29 03:02:54 -0600

    Added code to compute metrics for autoscaling and added logs for key information that can be used for debugging

--- test_autoscaling.py
+++ test_autoscaling.py
@@ -6,6 +6,7 @@ from hashring import HashRing
 import time
 import joblib
 import string
+import statistics
 
 # globals
 HOST = "localhost"
@@ -25,7 +26,7 @@ transition = False
 
 
 def launch_new_container():
-    print("New container launched")
+    # print("New container launched")
     current_port = available_ports.pop(0)
     url = f"http://{HOST}:{current_port}"
 
@@ -46,7 +47,6 @@ def launch_new_container():
         detach=True,
         ports={"80/tcp": current_port},
         mem_limit="15m",  # 10m
-        auto_remove=True,
     )
     # container = docker_client.containers.run(
     #     "docker-kv-store",
@@ -59,11 +59,11 @@ def launch_new_container():
     # time.sleep(3)
 
     container_id = container.id
-    print(container_id)
+    # print(container_id)
 
     servers[container_id] = url
     available_servers.append(url)
-    print(f"Avaliable servers: {available_servers}")
+    # print(f"Avaliable servers: {available_servers}")
 
     # update new hash ring, TODO: need lock mechinaism
     global ring
@@ -115,19 +115,19 @@ def no_space_in_container(num_write, num_read, rw_ratio):
     required_cpu, required_memory = get_resource_usage_prediction(
         num_read, num_write, rw_ratio
     )
-    print("required cpu: {:.2f}".format(required_cpu))
-    print("required memory: {:.2f}".format(required_memory))
+    # print("required cpu: {:.2f}".format(required_cpu))
+    # print("required memory: {:.2f}".format(required_memory))
 
     # Extract and print the names of running containers
     for container in containers:
         # print("Check")
-        print("Container Name:", container.name)
+        # print("Container Name:", container.name)
         stats = container.stats(stream=False)
 
         cpu_usage, memory_usage, memory_limit = get_cpu_memory_usage(stats)
-        print("cpu usage: {:.2f}".format(cpu_usage))
-        print("memory usage: {:.2f}".format(memory_usage))
-        print("memory limit: {:.2f}".format(memory_limit))
+        # print("cpu usage: {:.2f}".format(cpu_usage))
+        # print("memory usage: {:.2f}".format(memory_usage))
+        # print("memory limit: {:.2f}".format(memory_limit))
 
         # FIX LATER
         if (cpu_usage + required_cpu <= 100) and (
@@ -136,15 +136,18 @@ def no_space_in_container(num_write, num_read, rw_ratio):
             print("There is space to work on task")
             print()
             return False
-        print()
+        # print()
 
-    print("Unable to handle workload, launch the new container!!")
+    # print("Unable to handle workload, launch the new container!!")
     return True
 
 
 def client_ops(client_id, workload):
     num_write, num_read, rw_ratio = workload
     written_keys = []
+
+    errors = 0
+    successes = 0
 
     # predict the container cpu and memory usage
     # launch new container if needed
@@ -156,7 +159,7 @@ def client_ops(client_id, workload):
 
     for i in range(num_write):
         if transition:
-            time.sleep(5)
+            time.sleep(0.2)
             transition = False
         # print("Transition Put check: ", transition)
         key = f"key-{client_id}-{i}"  # NEED TO ADD RANDOMNESS IN KEY GENERATION AND VALUE GENERATION
@@ -168,24 +171,35 @@ def client_ops(client_id, workload):
 
         server_url = ring.get_node(key)
 
-        print(f"Write checkpoint - Server URL: {server_url}, Key: {key} Value: {value}")
+        # print(f"Write checkpoint - Server URL: {server_url}, Key: {key} Value: {value}")
+
+        # with open("autoscaling_logs.txt", "a") as f:
+        #     f.write(
+        #         f"Write checkpoint - Server URL: {server_url}, Key: {key} Value: {value}\n"
+        #     )
 
         try:
             response = requests.put(
                 f"{server_url}/store", params={"key": key}, data={"value": value}
             )
-            print(f"PUT response: {response.status_code}, {response.text}")
+            # print(f"PUT response: {response.status_code}, {response.text}")
+            # with open("autoscaling_logs.txt", "a") as f:
+            #     f.write(f"PUT response: {response.status_code}, {response.text}\n")
             if response.status_code == 404:
+                errors += 1
                 print("**" * 68)
                 print("Put Error")
-                break
+                # break
+            else:
+                successes += 1
             written_keys.append(key)
         except Exception as e:
+            errors += 1
             print(f"Error during PUT request: {e}")
 
     for j in range(num_read):
         if transition:
-            time.sleep(5)
+            time.sleep(0.2)
             transition = False
         # print("Transition Get check: ", transition)
         random_key_index = random.randint(0, len(written_keys) - 1)
@@ -193,16 +207,26 @@ def client_ops(client_id, workload):
 
         server_url = ring.get_node(key)
 
-        print(f"Get checkpoint - Server URL: {server_url}, Key: {key}")
+        # print(f"Get checkpoint - Server URL: {server_url}, Key: {key}")
+        # with open("autoscaling_logs.txt", "a") as f:
+        #     f.write(f"Get checkpoint - Server URL: {server_url}, Key: {key}\n")
         try:
             response = requests.get(f"{server_url}/retrieve", params={"key": key})
-            print(f"GET response: {response.status_code}, {response.text}")
+            # print(f"GET response: {response.status_code}, {response.text}")
+            # with open("autoscaling_logs.txt", "a") as f:
+            #     f.write(f"GET response: {response.status_code}, {response.text}\n")
             if response.status_code == 404:
+                errors += 1
                 print("**" * 68)
                 print("Get Error")
-                break
+                # break
+            else:
+                successes += 1
         except Exception as e:
+            errors += 1
             print(f"Error during GET request: {e}")
+
+    return errors, successes
 
 
 if __name__ == "__main__":
@@ -226,27 +250,59 @@ if __name__ == "__main__":
             for n_write, n_read, rw_ratio in workload
         ]
 
-    # throughputs = []
-    # latencies = []
+    throughputs = []
+    latencies = []
+    error_rates = []
+    all_reads = []
+    all_writes = []
+    all_rw_ratios = []
 
-    for client_id in range(len(workload)):
+    # for client_id in range(len(workload)):
+
+    for client_id in range(0, 10):
+        print(client_id)
         # print("Workload: ", workload)
         # print("Workload check: ", workload[client_id])
-        # num_reads, num_writes, read_write_ratio = workload[client_id]
-        # print("Num reads: ", num_reads)
-        # print("Num writes: ", num_writes)
-        # print("Num read_write_ratio: ", read_write_ratio)
-        print("***" * 68)
-        print("This is a new workload")
+        num_reads, num_writes, read_write_ratio = workload[client_id]
 
-        # with open("autoscaling_logs.txt", "w") as f:
-        #     f.write(f"This is a new workload that has the following num_read:\n")
+        all_reads.append(num_reads)
+        all_writes.append(num_writes)
+        all_rw_ratios.append(read_write_ratio)
+
+        # print("***" * 68)
+
+        # with open("autoscaling_logs.txt", "a") as f:
+        #     f.write("**" * 60 + "\n")
+        #     f.write(f"New Workload: {client_id}\n")
+        #     f.write("\n")
         # print("Workload: ", workload, len(workload))
         # break
-        # start = time.time()
-        client_ops(client_id, workload[client_id])
-        # total_time = time.time() - start
-        # throughput = NUM_OPS / total_time
-        # latency = total_time / NUM_OPS
+        start = time.time()
+        errors, succeses = client_ops(client_id, workload[client_id])
+        total_time = time.time() - start
+        NUM_OPS = num_reads + num_writes
+        throughput = NUM_OPS / total_time
+        latency = total_time / NUM_OPS
+        error_rate = errors / NUM_OPS
+        throughputs.append(throughput)
+        latencies.append(latency)
+        error_rates.append(error_rate)
+
+        # might remove the transition time or reduce it
     # remove all containers
     remove_all_containers()
+    with open("autoscaling_logs.txt", "a") as f:
+        for i in range(len(throughputs)):
+            f.write(
+                f"Client {i} NUM_READS: {all_reads[i]} NUM_WRITES: {all_writes[i]} READ_WRITE_RATIO: {all_rw_ratios[i]} Throughput: {throughputs[i]}, Latency: {latencies[i]}, Error Rate: {error_rates[i]}\n"
+            )
+
+    # Maybe add other stats too like std deviation, variation, etc
+    overall_average_throughput = statistics.mean(throughputs)
+    overall_average_latency = statistics.mean(latencies)
+    overall_average_errorRate = statistics.mean(error_rates)
+
+    with open("autoscaling_logs.txt", "a") as f:
+        f.write(
+            f": overall_average_throughput: {overall_average_throughput}, overall_average_latency: {overall_average_latency}, overall_average_errorRate: {overall_average_errorRate}\n"
+        )
