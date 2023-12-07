e4d2f52269dbf46925fe3008fcf6ba2e7fa0c711
sawashu <sawamurasatoko@Shun-MacBook.local>
2023-12-07 14:43:18 -0600

    client.py for go

--- client.py
+++ client.py
@@ -1,58 +1,85 @@
+import multiprocessing
 import requests
 import time
-import threading
+import random
+import multiprocessing
+import requests
+import time
+import random
+from hashring import HashRing
+BASE_URLS = ['http://127.0.0.1:8070', 'http://127.0.0.1:8080', 'http://127.0.0.1:8090']
+#BASE_URLS = ['http://127.0.0.1:8080', 'http://127.0.0.1:8081']
+#BASE_URLS = 'http://localhost:80'
+ring = HashRing(BASE_URLS)
 
-HOST, PORT = "127.0.0.1", 80
-BASE_URL = f"http://{HOST}:{PORT}"
-NUM_REQUESTS = 1000  # Number of requests to send per client
-NUM_CLIENTS = 10  # Number of concurrent clients
 
-throughputs = []
-latencies = []
+def kv_store_client(operation, key, value=None):
+    # Determine which server to use based on the key
+    server_url = ring.get_node(key)
+    #server_url = BASE_URLS
+    start_time = time.time()
+    try:
+        if operation == 'set':
+            response = requests.put(f'{server_url}/store', json={key: value})
+        elif operation == 'get':
+            response = requests.get(f'{server_url}/retrieve?key={key}')
+        else:
+            raise ValueError('Invalid operation')
+        end_time = time.time()
+        return end_time - start_time
+    except Exception as e:
+        print(f'Error performing {operation} on {key}: {e}')
+        return None
 
+def worker(num_operations, latencies_queue, operation, process_index):
+    for i in range(num_operations):
+        key = f'key{process_index}_{i}'
+        value = f'value{process_index}_{i}' if operation == 'set' else None
+        latency = kv_store_client(operation, key, value)
+        if latency is not None:
+            latencies_queue.put(latency)
 
-def client_thread(client_id):
-    # Start time
+def benchmark(num_operations, num_processes):
+    manager = multiprocessing.Manager()
+    latencies_queue = manager.Queue()
+
+    set_processes = [
+        multiprocessing.Process(target=worker, args=(num_operations, latencies_queue, 'set', i))
+        for i in range(num_processes)
+    ]
+    get_processes = [
+        multiprocessing.Process(target=worker, args=(num_operations, latencies_queue, 'get', i))
+        for i in range(num_processes)
+    ]
     start_time = time.time()
 
-    for i in range(NUM_REQUESTS):
-        key = f"key-{client_id}-{i}"
-        value = f"value-{client_id}-{i}"
+    for p in set_processes:
+        p.start()
+    for p in set_processes:
+        p.join()
 
-        # PUT request
-        requests.put(f"{BASE_URL}/store", data={"key": key, "value": value})
+    for p in get_processes:
+        p.start()
+    for p in get_processes:
+        p.join()
 
-        # GET request
-        requests.get(f"{BASE_URL}/retrieve", data={"key": key})
+    total_time = time.time() - start_time
+    total_operations = num_operations * num_processes * 2  # Each process does num_operations SET and GET
+    total_latencies = []
 
-        # DEL request
-        requests.delete(f"{BASE_URL}/remove", data={"key": key})
+    while not latencies_queue.empty():
+        total_latencies.append(latencies_queue.get())
 
-    # End time
-    end_time = time.time()
+    average_latency = sum(total_latencies) / len(total_latencies)
+    print(f'Total Latency: {sum(total_latencies):.2f} second')
+    print(f'Length of latencies: {len(total_latencies):.0f} latencies')
+    throughput = total_operations / total_time
+    print(f'Total Operation: {total_operations:.0f} operations')
+    print(f'Average Latency: {average_latency:.5f} seconds per operation')
+    print(f'Throughput: {throughput:.2f} operations per second')
+    print(f'Total Benchmark Time: {total_time:.2f} seconds')
 
-    elapsed_time = end_time - start_time
-    throughput = NUM_REQUESTS * 3 / elapsed_time  # Multiply by 3 for PUT, GET, DEL
-    throughputs.append(throughput)
-    latency = elapsed_time / (NUM_REQUESTS * 3)  # Average latency per request
-    latencies.append(latency)
-
-    print(
-        f"Client-{client_id} | Throughput: {throughput:.2f} req/s | Average Latency: {latency:.6f} seconds"
-    )
-
-
-if __name__ == "__main__":
-    clients = []
-    for i in range(NUM_CLIENTS):
-        t = threading.Thread(target=client_thread, args=(i,))
-        clients.append(t)
-        t.start()
-
-    for t in clients:
-        t.join()
-
-    print("Overall Throughput: ", sum(throughputs) / NUM_CLIENTS)
-    print("Overall Latency: ", sum(latencies) / NUM_CLIENTS)
-
-    print("Testing completed.")
+if __name__ == '__main__':
+    num_operations_per_process = 800
+    num_processes = 10
+    benchmark(num_operations_per_process, num_processes)
