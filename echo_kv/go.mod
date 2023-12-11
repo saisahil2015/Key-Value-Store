9e1b2492c07ac4f7b6addbac7608792d4d4e7b03
Sahil Raina <saisahil2015@gmail.com>
2023-12-10 21:32:27 -0600

    Unnecesary files removed

--- echo_kv/go.mod
+++
@@ -1,5 +0,0 @@
-module sample
-
-go 1.14
-
-require github.com/labstack/echo/v4 v4.1.16 // indirect
--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class Key_value(db.Model):
-    key = db.Column(db.String(100), primary_key=True)
-    value = db.Column(db.String(100), nullable=False)
